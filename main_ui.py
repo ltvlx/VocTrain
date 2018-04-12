7454e92b1051eb8c18d952f919585dfb51a596f0
ltvlx <lyutov.alexey@gmail.com>
2018-04-12 21:56:39 +0200

    geometry parametrization

--- main_ui.py
+++ main_ui.py
@@ -11,43 +11,62 @@ class Window(QtWidgets.QWidget):
         self.init_ui()
         
     def init_ui(self):
-        self.setFixedSize(560, 550)
+        # basic geometry parameters of window and widgets
+        margin = 25
+        main_w, main_h = 560, 550
+        btn_h = 60
+        out_h = 200
+        in_h = 100
+        bar_h = 25
+
+        # other parameters that are automatically calculated from basic ones
+        in_w = out_w = main_w - 2 * margin
+        in_y = margin * 2 + out_h
+        btn_y =  in_y + in_h + margin
+        btn_s_w = 0.4 * main_w
+        btn_s_x = 0.3 * main_w
+        btn_1_w = (main_w - margin * 3) * 0.4
+        btn_2_w = (main_w - margin * 3) * 0.6
+        btn_1_x = margin
+        btn_2_x = btn_1_x + btn_1_w + margin
+
+
+        self.setFixedSize(main_w, main_h)
         self.setWindowTitle("Train vocabulary")
         
         self.f_out = QtWidgets.QTextBrowser(self)
-        self.f_out.setGeometry(30, 30, 500, 200)
+        self.f_out.setGeometry(margin, margin, out_w, out_h)
         self.f_out.setStyleSheet("""border: 1px solid; font: Arial; font-size: 16px;""")
 
         self.f_in = QtWidgets.QTextEdit(self)
-        self.f_in.setGeometry(30, 260, 500, 100)
+        self.f_in.setGeometry(margin, in_y, in_w, in_h)
         self.f_in.setStyleSheet("""border: 1px solid; font: Arial; font-size: 16px;""")
 
-        # Start button
         self.b_start = QtWidgets.QPushButton(self)
-        self.b_start.setGeometry(160, 390, 240, 60)
+        self.b_start.setGeometry(btn_s_x, btn_y, btn_s_w, btn_h)
         self.b_start.setStyleSheet("""background-color: rgb(170, 170, 170); font: bold Arial; font-size: 18px;""")
         self.b_start.setText("Select vocabulary \nand start training")
         self.b_start.clicked.connect(self.start_training)
 
-        # Answer button
-        self.b_answer = QtWidgets.QPushButton(self)
-        self.b_answer.setGeometry(210, 390, 320, 60)
-        self.b_answer.setStyleSheet("""background-color: rgb(100, 170, 100); font: bold Arial; font-size: 18px;""")
-        self.b_answer.setText("answer")
-        self.b_answer.clicked.connect(self.event_answer)
-        self.b_answer.hide()
-
         # Stop button
         self.b_stop = QtWidgets.QPushButton(self)
-        self.b_stop.setGeometry(30, 390, 150, 60)
+        self.b_stop.setGeometry(btn_1_x, btn_y, btn_1_w, btn_h)
         self.b_stop.setStyleSheet("""background-color: rgb(180, 60, 60); font: bold Arial; font-size: 18px;""")
         self.b_stop.setText("save and \nexit")
         self.b_stop.clicked.connect(self.event_stop)
         self.b_stop.hide()
 
+        # Answer button
+        self.b_answer = QtWidgets.QPushButton(self)
+        self.b_answer.setGeometry(btn_2_x, btn_y, btn_2_w, btn_h)
+        self.b_answer.setStyleSheet("""background-color: rgb(100, 170, 100); font: bold Arial; font-size: 18px;""")
+        self.b_answer.setText("answer")
+        self.b_answer.clicked.connect(self.event_answer)
+        self.b_answer.hide()
+
         # Next button
         self.b_next = QtWidgets.QPushButton(self)
-        self.b_next.setGeometry(210, 390, 320, 60)
+        self.b_next.setGeometry(btn_2_x, btn_y, btn_2_w, btn_h)
         self.b_next.setStyleSheet("""background-color: rgb(100, 100, 100); font: bold Arial; font-size: 18px;""")
         self.b_next.setText("next word")
         self.b_next.clicked.connect(self.event_next)
@@ -55,7 +74,7 @@ class Window(QtWidgets.QWidget):
 
         # Stats panel
         self.stats = QtWidgets.QLabel(self)
-        self.stats.setGeometry(-2, 525, 564, 27)
+        self.stats.setGeometry(-2, main_h - bar_h, main_w + 4, bar_h + 2)
         self.stats.setAlignment(QtCore.Qt.AlignRight)
         self.stats.setStyleSheet("""border: 2px solid rgb(150, 150, 150); font: Arial; font-size: 16px;""")
         self.stats.hide()
@@ -66,7 +85,7 @@ class Window(QtWidgets.QWidget):
 
         # Stats button
         self.b_stats = QtWidgets.QPushButton(self)
-        self.b_stats.setGeometry(-2, 525, 100, 27)
+        self.b_stats.setGeometry(-2, main_h - bar_h, 100, bar_h + 2)
         self.b_stats.setStyleSheet("""border: 2px solid rgb(150, 150, 150); background-color: rgb(240, 240, 240); font: Arial; font-size: 16px;""")
         self.b_stats.setText("show stats")
         self.b_stats.hide()
@@ -80,8 +99,9 @@ class Window(QtWidgets.QWidget):
         self.fname = self.select_xlsx()
         
         k_train = self.select_mode()
+        
         self.vocab = vt.VocabularyTrainer(self.fname, k_train)
-
+        
         self.stats.setText(self.vocab.get_status())
 
         self.b_start.hide()
@@ -96,11 +116,11 @@ class Window(QtWidgets.QWidget):
 
 
     def select_xlsx(self):
-        fname_selected = QtWidgets.QFileDialog.getOpenFileName(directory='./', filter='Vocabulary in excel table (*.xlsx *.xls)')[0]
-        if fname_selected == '':
-            self.close()
-        else:
-            return fname_selected
+        fname =  QtWidgets.QFileDialog.getOpenFileName(directory='./', filter='Vocabulary in excel table (*.xlsx *.xls)')[0]
+        if fname == '':
+            print("No file selected! Program exit!")
+            sys.exit()
+        return fname
 
 
     def select_mode(self):
